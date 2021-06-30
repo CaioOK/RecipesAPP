593bcbb0386ff77cbeb342c1c86307869f42b5cb
rcalvs <rafaelcalvette@gmail.com>
2021-06-30 16:53:06 -0300

    CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import '../App.css';
 
 function Footer() {
   return (
