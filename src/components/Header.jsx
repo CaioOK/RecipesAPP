903518165bc256e97b44024befe49408c308cbac
Lucas André <lucas.cancado89@gmail.com>
2021-06-30 17:29:45 -0300

    Feat-Req11-Profile button redirects to profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
 
 function Header({ pageTitle, searchFeat }) {
   const createSearchIcon = () => (
-    <i src="../images/searchIcon.svg" data-testid="search-top-btn">
-      <img src={ searchIcon } alt="search icon" />
-    </i>
+    <button type="button">
+      <img
+        data-testid="search-top-btn"
+        src={ searchIcon }
+        alt="search icon"
+      />
+    </button>
   );
 
   return (
     <div>
       <h1 data-testid="page-title">{ pageTitle }</h1>
-      <i src="../images/profileIcon.svg" data-testid="profile-top-btn">
-        <img src={ profileIcon } alt="profile icon" />
-      </i>
+      <Link
+        to="/perfil"
+      >
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="profile icon"
+        />
+      </Link>
       {
         searchFeat
           ? createSearchIcon()
